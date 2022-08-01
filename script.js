70d057eac927fdd0a5597b597d19915f7a0390f4
Lee <leewhiz119@gmail.com>
2022-08-01 11:00:04 -0400

    added appendnumber function

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ class Calculator {
 }
 
 	appendNumber(number) {
-		this.currentOperand = number
+		this.currentOperand = this.currentOperand.toString() + number.toString()
 }
 
 	chooseOperation(operation) {
