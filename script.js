a54fed06eb5d82e53d0fa59c37a86bd6f55eddca
Lee <leewhiz119@gmail.com>
2022-08-09 11:17:46 -0400

    added delete button eventlistener

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ class Calculator {
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
-const deleteButtons = document.querySelector('[data-delete]')
+const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
@@ -92,5 +92,10 @@ equalsButton.addEventListener('click', button => {
 
 allClearButton.addEventListener('click', button => {
 	calculator.clear()
+	calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+	calculator.delete()
 	calculator.updateDisplay()
 })
